36e7cf46ff17b4164f41cfaab2c409b6a87dee58
laura León <isabelaleon8@gmail.com>
2020-05-18 22:46:11 -0500

    subi las clases faltantes

---
+++ src/Model/PokemonThree.java
@@ -0,0 +1,12 @@
+package Model;
+import processing.core.PApplet;
+public class PokemonThree extends Pokemon{
+		
+		public PokemonThree(PApplet app) {
+			super(app);
+		}
+		
+		public void pintar() {
+			
+		}
+}
