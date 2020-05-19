3656de90f787960d2911d0cd6b7df10cfa8c254f
nataliaortizvi <natiorvi@hotmail.com>
2020-05-18 23:40:12 -0500

    paquete excepciones y una clase que faltaba

---
+++ src/LaExcepcion/ExceptionNombre.java
@@ -0,0 +1,10 @@
+package LaExcepcion;
+
+public class ExceptionNombre extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionNombre (String msg) {
+		super(msg);
+	}
+}
--- src/Model/Pokedex.java
+++ src/Model/Pokedex.java
@@ -1,5 +1,14 @@
 package Model;
 
+import processing.core.PApplet;
+
 public class Pokedex {
+	
+	String nom;
+	String tipo;
+	
+	public Pokedex (String nom, String tipo, PApplet app) {
+		
+	}
 
 }
